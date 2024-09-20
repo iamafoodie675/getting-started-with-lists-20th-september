aebd38e0ce8240e11bfe9e6a15cccfd64820fd1c
nabila ahmed <sumayaad2020@gmail.com>
2024-09-20 19:06:15 +0600

    first commit

---
+++ list.py
@@ -0,0 +1,16 @@
+names = ["Nusaiba","Nabila", "Rose"]
+print(names)
+
+print(names[0])
+print(names[1])
+print(names[2])
+
+names.append("Nahian sir")
+
+print(names[3])
+print(names)
+
+print(len(names))
+
+aList = [100,200,300,400,500]
+print(aList[::-1])
